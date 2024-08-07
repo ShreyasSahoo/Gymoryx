8caae293d3cec30e392fd92c82cbd1d581da8b2d
Shreyas Sahoo <shreyassahoo26@gmail.com>
2024-08-07 13:22:22 +0530

    Initial Commit

---
+++ Gymoryx/GymoryxApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymoryxApp.swift
+//  Gymoryx
+//
+//  Created by Shreyas Sahoo on 07/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GymoryxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
