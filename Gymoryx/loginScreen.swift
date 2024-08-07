ae70dc414306945be13c4ed23c0e17f98ff7deff
Divyansh-Kaushik84 <84divyanshkaushik@gmail.com>
2024-08-08 03:25:23 +0530

    completed SetGoal  Screen , SetBodyMeasurement Screen and login screen was already done.

--- Gymoryx/loginScreen.swift
+++ Gymoryx/loginScreen.swift
@@ -19,13 +19,12 @@ struct loginScreen: View {
     
                 Image("gymoryx")
                     .resizable()
-                    .frame(width: 175,height: 200)
-                    
+                    .frame(width: UIScreen.main.bounds.width/2,height: UIScreen.main.bounds.height/4)
                 VStack(alignment:.leading){
                     
                     Text("Enter Email")
                         .fontWeight(.bold)
-                        .foregroundColor(Color(red: 0.4, green: 0.4, blue: 0.4))
+                        .foregroundColor(Color(.gray))
                         .padding(.bottom)
                     
                     TextField("", text: $email)
@@ -38,7 +37,7 @@ struct loginScreen: View {
                     
                     Text("Enter Password")
                         .fontWeight(.bold)
-                        .foregroundColor(Color(red: 0.4, green: 0.4, blue: 0.4))
+                        .foregroundColor(Color(.gray))//red: 0.4, green: 0.4, blue: 0.4
                         .padding(.vertical)
                     
                     SecureField("", text: $password)
@@ -57,8 +56,8 @@ struct loginScreen: View {
                 .font(.callout)
                 .fontWeight(.bold)
                 .padding(.leading,175)
-                .accentColor(Color(red: 0.132, green: 0.338, blue: 0.512))
-                  
+                .foregroundColor(Color("navyblue"))
+
                 NavigationLink(
                         destination: Text("Login"),label:
                 {
@@ -70,14 +69,15 @@ struct loginScreen: View {
                     })
                 .frame(width: 300,height: 60)
                 .padding(.horizontal,5)
-                .background(Color(red: 0.132, green: 0.338, blue: 0.512))
+                .background(Color("navyblue"))
                 .cornerRadius(15)
 
                 NavigationLink(
                     destination: Text("No account"),label: {Text("Don't have an account? Sign Up")
                         .font(.subheadline)
                         .fontWeight(.heavy)
-                        .accentColor(Color(red: 0.132, green: 0.338, blue: 0.512))})
+                        .foregroundColor(Color("navyblue"))
+                    })
                 .padding([.top,.horizontal]
                 )
                 
@@ -90,7 +90,6 @@ struct loginScreen: View {
                     Text("OR")
                         .foregroundColor(.gray)
                         .font(.headline)
-                        .padding(.horizontal, 10)
                     VStack {
                         Divider()
                             .frame(height: 2)
@@ -111,7 +110,7 @@ struct loginScreen: View {
                         
                         Text("Connect with Google")
                     }
-                    .foregroundColor(Color(red: 0.4, green: 0.4, blue: 0.4))
+                    .foregroundColor(Color(.gray))//red: 0.4, green: 0.4, blue: 0.4 can also use this
                     .fontWeight(.heavy)
                     .font(.callout)
                     .frame(width: 275,height: 50)
@@ -123,7 +122,6 @@ struct loginScreen: View {
                     }
                 }
             }.padding()
-            .padding(.bottom,100)
         }
     }
 }
