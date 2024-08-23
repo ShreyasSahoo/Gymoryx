5d37e8dc9447a549147135c1edfe0a5bdd2f93fe
Shreyas Sahoo <shreyassahoo26@gmail.com>
2024-08-23 21:20:28 +0530

    feat : fixed login and added maintabview and homeview

---
+++ Gymoryx/Views/Main/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  Gymoryx
+//
+//  Created by Shreyas Sahoo on 23/08/24.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
